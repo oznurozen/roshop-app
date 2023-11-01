8e6632379b161980f61b62dc423e1798684cfca3
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2023-11-01 18:21:57 +0300

    update

--- src/redux/cardSlice.js
+++ src/redux/cardSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const fetchFromLocalStorage = () => {
-    let cart = localStorage.getItem('cart');
+const fetchFromLocalStorage = () => { // cart ile ilgili verileri her işlem yaptığımda set edicem. set ettiğim seyleri localstarge üzerinden alıcam 
+    let cart = localStorage.getItem('cart'); // ve json.pars ederek return edicem
     if(cart){
         return JSON.parse(localStorage.getItem('cart'))
     }else{
@@ -10,57 +10,59 @@ const fetchFromLocalStorage = () => {
     }
 }
 
-const storeInLocalStorage = (data) => {
+const storeInLocalStorage = (data) => { //dışarıdan veri alıcak gelen veri üzerinden json.stringify kullanarak bunları setıtem üzerinden harekete geçiricez
     localStorage.setItem('cart', JSON.stringify(data))
 }
 
 const initialState = {
-    carts: fetchFromLocalStorage(),
-    itemCount: 0,
-    totalAmount: 0
+    carts: fetchFromLocalStorage(), //hesaplamaları yapacağım olay
+    itemCount: 0, //ürün sayım başlangıç degeri
+    totalAmount: 0 //toplam tutar
 }
 
 const cartSlice =  createSlice({
     name : "cart",
     initialState,
     reducers: {
-        addToCart: (state, action) => {
-            const isItemCart = state.carts.find(item => item.id === action.payload.id)
+        addToCart: (state, action) => { //kart ekleme kısımları
+            const isItemCart = state.carts.find(item => item.id === action.payload.id) 
+            //state.cards üzerinden bul=find et. her bir elemanını tek tek gez, dışarıdan gelen id değerinin eşit olup olmadığını kontrol et bul 
+            //aynı ürün varsa ekrana koy yada koyma gibi düşünebiliriz
 
             if(isItemCart){
-                const tempCart = state.carts.map(item => {
-                    if(item.id === action.payload.id){
+                const tempCart = state.carts.map(item => { //state in kartlarını tek tek gezicem
+                    if(item.id === action.payload.id){ //her bir kart içerisinden gelen itemların id leri ile action.payload.id değerler eşitse
                         let tempQty = item.quantity + action.payload.quantity;
                         let tempTotalPrice = tempQty + item.price;
                         return{
                             ...item, quantity: tempQty, totalPrice : tempTotalPrice
                         }
                     }else{
-                        return item
+                        return item //birbirlerine eşit değilse item ı dönsün
                     }
                 })
 
                 state.carts = tempCart;
                 storeInLocalStorage(state.carts)
             }else{
-                state.carts.push(action.payload)
-                storeInLocalStorage(state.carts)
+                state.carts.push(action.payload) //böyle bir ürün olmama durumunda ction.payload puslanır
+                storeInLocalStorage(state.carts) //pusladıktan sonra da storeInLocalStorage içerisine kaydetmeyı yapacağız
             }
         },
-        removeFromCart : (state, action) => {
-            const tempCart = state.carts.filter(item => item.id !== action.payload)
-            state.carts = tempCart
-            storeInLocalStorage(state.carts)
+        removeFromCart : (state, action) => { //kart silme kısmı
+            const tempCart = state.carts.filter(item => item.id !== action.payload) //farklı olanları filtrele aynı olanları sil
+            state.carts = tempCart  //state.carts larımı  güncellemem gerekiyor
+            storeInLocalStorage(state.carts) //localstorage içerisini de güncellemek istiyorum
         },
-        clearCart : (state) => {
+        clearCart : (state) => { //tüm kartımı temizlemek
             state.carts = []
             storeInLocalStorage(state.carts)
         },
-        getCartTotal : (state) => {
+        getCartTotal : (state) => { //toplam fiyatları ve ürün sayısını döndürmeye çalışabiliriz.
             state.totalAmount = state.carts.reduce((cartTotal, cartItem)=>{
-                return cartTotal += cartItem.price * cartItem.quantity
+                return cartTotal += cartItem.price * cartItem.quantity // sepetteki toplam fiyatlar
             }, 0)
-            state.itemCount = state.carts.length
+            state.itemCount = state.carts.length //kartımda kaç ürün var
         }
 
     }
