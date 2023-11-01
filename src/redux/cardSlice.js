4f1426231fc451262f2233aa0172e31ad92d6fd8
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2023-11-01 18:57:07 +0300

    update

--- src/redux/cardSlice.js
+++ src/redux/cardSlice.js
@@ -1,18 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+//fetchFromLocalStorage = yerel depodan getir anlamında
 const fetchFromLocalStorage = () => { // cart ile ilgili verileri her işlem yaptığımda set edicem. set ettiğim seyleri localstarge üzerinden alıcam 
     let cart = localStorage.getItem('cart'); // ve json.pars ederek return edicem
     if(cart){
         return JSON.parse(localStorage.getItem('cart'))
     }else{
-        return []
+        return []  //bu benim için verileri ÇAĞARDIĞIM YER
     }
 }
 
 const storeInLocalStorage = (data) => { //dışarıdan veri alıcak gelen veri üzerinden json.stringify kullanarak bunları setıtem üzerinden harekete geçiricez
-    localStorage.setItem('cart', JSON.stringify(data))
-}
+    localStorage.setItem('cart', JSON.stringify(data)) // dışarıdan vermiş olduğum verileri localStorage bir fonksiyon olarak düşünebilirsiniz.
+} 
 
 const initialState = {
     carts: fetchFromLocalStorage(), //hesaplamaları yapacağım olay
@@ -32,19 +32,21 @@ const cartSlice =  createSlice({
             if(isItemCart){
                 const tempCart = state.carts.map(item => { //state in kartlarını tek tek gezicem
                     if(item.id === action.payload.id){ //her bir kart içerisinden gelen itemların id leri ile action.payload.id değerler eşitse
-                        let tempQty = item.quantity + action.payload.quantity;
-                        let tempTotalPrice = tempQty + item.price;
-                        return{
+                        let tempQty = item.quantity + action.payload.quantity; // bir tane dışarıdan gönderdiğim quantity
+                        let tempTotalPrice = tempQty + item.price; //oluşturmuş olduğum quantity ile beraber price ı hesapladı toplam
+                        // total hesaplama işlemlerini yapmış olucam burada
+                        return{ //bunları yapptıktan sonra return etmemiz gerekiyor
                             ...item, quantity: tempQty, totalPrice : tempTotalPrice
+                            //tüm item ları dön, döndüğün itemler üzerinden bir quantity olayı döndür , en sonda totalPrice hesaplaması yap
                         }
-                    }else{
+                    }else{ //eşit değilse!!!
                         return item //birbirlerine eşit değilse item ı dönsün
                     }
                 })
 
-                state.carts = tempCart;
-                storeInLocalStorage(state.carts)
-            }else{
+                state.carts = tempCart; //state.carts ların içerisine tempCartları atıyoruz
+                storeInLocalStorage(state.carts) //fonfsiyonumun içerisine de carts ı set ediyorum
+            }else{ //isItemCart yoksa!!!
                 state.carts.push(action.payload) //böyle bir ürün olmama durumunda ction.payload puslanır
                 storeInLocalStorage(state.carts) //pusladıktan sonra da storeInLocalStorage içerisine kaydetmeyı yapacağız
             }
